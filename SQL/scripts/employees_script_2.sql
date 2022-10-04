c65196eec1f10527ab8453185c935562dcea18cd
unknown <eldrigeampong@gmail.com>
2022-10-04 22:35:57 +0200

    modified files

--- SQL/scripts/employees_script_2.sql
+++ SQL/scripts/employees_script_2.sql
@@ -1,39 +1,31 @@
-use employees;
 
-SELECT DISTINCT
+/* Employees Script 2 */
+
+select
+	distinct
     e.emp_no,
-    e.first_name,
-    e.last_name,
-    e.gender,
-    t.title,
-    s.salary,
-    de.dept_no,
-    d.dept_name
-FROM
-    employees e
-        LEFT JOIN
-    titles t ON e.emp_no = t.emp_no
-        LEFT JOIN
-    salaries s ON e.emp_no = s.emp_no
-        LEFT JOIN
-    dept_emp de ON e.emp_no = de.emp_no
-        LEFT JOIN
-    departments d ON de.dept_no = d.dept_no
-GROUP BY 1
-ORDER BY 1 ASC
-
-
-
-/*
-
-select * from employees e;
-select * from titles t;
-select * from salaries s;
-select * from dept_emp de;
-select * from departments d;
-
-*/
-
-
-
-
+	e.first_name,
+	e.last_name,
+	e.gender,
+	t.title,
+	s.salary,
+	de.dept_no,
+	d.dept_name
+from
+	employees e
+left join
+    titles t on
+	e.emp_no = t.emp_no
+left join
+    salaries s on
+	e.emp_no = s.emp_no
+left join
+    dept_emp de on
+	e.emp_no = de.emp_no
+left join
+    departments d on
+	de.dept_no = d.dept_no
+group by
+	1
+order by
+	1 asc;
