a6c1efde4ed6277e13433f0705bb81e3e22de7de
unknown <eldrigeampong@gmail.com>
2023-05-20 02:25:12 +0200

    added bike share project

--- Queries/ml.py
+++ Queries/ml.py
@@ -18,3 +18,33 @@ order by
 	1 asc
     
          '''
+
+
+
+query2 = '''
+         -- Returns bike rental history overtime
+
+with
+    bike_rental_tbl as (
+                        select 
+                              distinct 
+                                      bs.trip_id,
+                                      bs.start_time,
+                                      bs.end_time,
+                                      bs.duration,
+                                      bs.start_lat,
+                                      bs.start_lon,
+                                      bs.end_lat,
+                                      bs.end_lon,
+                                      bs.plan_duration,
+                                      bs.trip_route_category 
+                        from bike_share bs
+                       )
+select
+	  distinct *
+from
+	bike_rental_tbl
+order by
+	start_time asc
+    
+         '''
